ecbfd23f38cb0ae0ffe1fce77c7c693d42142000
Vincent SAILLARD <vincent.saillard@hotmail.fr>
2023-11-18 19:33:53 +0100

    responsive on header and home page

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -2,11 +2,17 @@ import "../header/header.css";
 import logo from "../../pictures/logo.png";
 import star from "../../pictures/star.png";
 import glass from "../../pictures/glass.png";
+import burger from "../../pictures/burger.png";
+import cross from "../../pictures/cross.png";
+
 import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Header = ({ setSearchName, token, setRegisterModalState }) => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [displayBurger, setDisplayBurger] = useState(false);
 
   return (
     <>
@@ -72,7 +78,98 @@ const Header = ({ setSearchName, token, setRegisterModalState }) => {
             >
               <img src={star} alt="captain america's star" className="star" />
             </div>
+
+            {displayBurger ? (
+              <div
+                className="burgermenu"
+                onClick={() => {
+                  setDisplayBurger(false);
+                }}
+              >
+                <img src={cross} alt="burger menu button" />
+              </div>
+            ) : (
+              <div
+                className="burgermenu"
+                onClick={() => {
+                  setDisplayBurger(true);
+                }}
+              >
+                <img src={burger} alt="burger menu button" />
+              </div>
+            )}
           </div>
+
+          {displayBurger ? (
+            <>
+              <div className={`linetwo ${displayBurger ? null : "none"}`}>
+                <div className="navBurger">
+                  <div className="border"></div>
+                  <Link
+                    to="/characters"
+                    className={`link ${
+                      location.pathname === "/characters" && "active"
+                    }`}
+                  >
+                    Heroes
+                  </Link>
+                  <div className="border"></div>
+                  <Link
+                    to="/comics"
+                    className={`link ${
+                      location.pathname === "/comics" && "active"
+                    }`}
+                  >
+                    Comics
+                  </Link>
+                  <div className="border"></div>
+                  <Link
+                    to="/favs"
+                    className={`link ${
+                      location.pathname === "/favs" && "active"
+                    }`}
+                  >
+                    Fav's
+                  </Link>
+                  <div className="border"></div>
+                </div>
+              </div>
+
+              <div
+                className={`linethree ${
+                  location.pathname === "/characters" ||
+                  location.pathname === "/comics"
+                    ? null
+                    : "none"
+                }`}
+              >
+                {/* On Comics or Characters pages search input is visible */}
+                <div
+                  className={`searchBurger ${
+                    location.pathname === "/characters" ||
+                    location.pathname === "/comics"
+                      ? null
+                      : "none"
+                  }`}
+                >
+                  <img
+                    src={glass}
+                    alt="white magnifying glass"
+                    className="glass"
+                  />
+                  <input
+                    type="search"
+                    id="search"
+                    className="searchBar"
+                    onChange={(event) => {
+                      const value = event.target.value;
+                      setSearchName(value);
+                    }}
+                  />
+                </div>
+              </div>
+            </>
+          ) : null}
         </div>
       </header>
     </>
